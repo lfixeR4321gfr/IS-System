123ca558894fad9d835dafa58df5433aa2646c1d
lfixeR4321gfr <allyfaki162@gmail.com>
2026-03-01 17:19:37 +0300

    Update database settings to use PostgreSQL
    Changed database configuration from SQLite to PostgreSQL.

--- bookingproject/settings.py
+++ bookingproject/settings.py
@@ -84,11 +84,18 @@ WSGI_APPLICATION = 'bookingproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookingproject',
+        'USER': 'postgres',
+        'PASSWORD': '4321',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 # register DB from sqlite3 to postgresql
 
        # 'ENGINE': 'django.db.backends.postgresql',
@@ -96,8 +103,8 @@ DATABASES = {
         #'PASSWORD':'4321',
         #'HOST':'localhost',
         #'PORT':'5432',
-    }
-}
+  #  }
+#}
 
 
 # Password validation
